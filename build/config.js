dc1803ef4d2eb2a4a47d60e2e4eeeeff45e28e7d
evanliu2968 <evanliu2968@gmail.com>
2018-09-12 00:00:09 +0800

    Add the origin version.

---
+++ build/config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var fs = require('fs');
+var nodeExternals = require('webpack-node-externals');
+var Components = require('../components.json');
+var saladConfig = require('./salad.config.json');
+
+var utilsList = fs.readdirSync(path.resolve(__dirname, '../src/utils'));
+var mixinsList = fs.readdirSync(path.resolve(__dirname, '../src/mixins'));
+var transitionList = fs.readdirSync(path.resolve(__dirname, '../src/transitions'));
+var externals = {};
+
+Object.keys(Components).forEach(function(key) {
+  externals[`vue-component/packages/${key}`] = `vue-component/lib/${key}`;
+});
+
+externals['vue-component/src/locale'] = 'vue-component/lib/locale';
+utilsList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`vue-component/src/utils/${file}`] = `vue-component/lib/utils/${file}`;
+});
+mixinsList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`vue-component/src/mixins/${file}`] = `vue-component/lib/mixins/${file}`;
+});
+transitionList.forEach(function(file) {
+  file = path.basename(file, '.js');
+  externals[`vue-component/src/transitions/${file}`] = `vue-component/lib/transitions/${file}`;
+});
+
+externals = [Object.assign({
+  vue: 'vue'
+}, externals), nodeExternals()];
+
+exports.externals = externals;
+
+exports.alias = {
+  main: path.resolve(__dirname, '../src'),
+  packages: path.resolve(__dirname, '../packages'),
+  examples: path.resolve(__dirname, '../examples'),
+  'vue-component': path.resolve(__dirname, '../')
+};
+
+exports.vue = {
+  root: 'Vue',
+  commonjs: 'vue',
+  commonjs2: 'vue',
+  amd: 'vue'
+};
+
+exports.jsexclude = /node_modules|utils\/popper\.js|utils\/date.\js/;
+
+exports.postcss = function(webapck) {
+  saladConfig.features.partialImport = {
+    addDependencyTo: webapck
+  };
+  return [
+    require('postcss-salad')(saladConfig)
+  ];
+};
