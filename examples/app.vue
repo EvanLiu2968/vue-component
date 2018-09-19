fb23988ec890e201aeec6bb54d56a33a95a16547
evanliu2968 <evanliu2968@gmail.com>
2018-09-20 03:33:46 +0800

    remove jump tips.

--- examples/app.vue
+++ examples/app.vue
@@ -214,64 +214,15 @@
 
     watch: {
       lang(val) {
-        if (val === 'zh-CN') {
-          this.suggestJump();
-        }
         localize(val);
       }
     },
 
     methods: {
-      suggestJump() {
-        if (process.env.NODE_ENV !== 'production') return;
-
-        const href = location.href;
-        const preferGithub = localStorage.getItem('PREFER_GITHUB');
-        if (href.indexOf('element-cn') > -1 || href.indexOf('element.faas') > -1 || preferGithub) return;
-        setTimeout(() => {
-          if (this.lang !== 'zh-CN') return;
-          this.$confirm('建议大陆用户访问部署在国内的站点，是否跳转？', '提示')
-            .then(() => {
-              location.href = location.href
-                .replace('https:', 'http:')
-                .replace('element.', 'element-cn.');
-            })
-            .catch(() => {
-              localStorage.setItem('PREFER_GITHUB', 'true');
-            });
-        }, 1000);
-      }
     },
 
     mounted() {
       localize(this.lang);
-      if (this.lang === 'zh-CN') {
-        this.suggestJump();
-      }
-      // setTimeout(() => {
-      //   const notified = localStorage.getItem('ES_NOTIFIED_2');
-      //   if (!notified && this.lang !== 'es') {
-      //     const title = this.lang === 'zh-CN'
-      //       ? '西班牙语文档正式上线'
-      //       : 'Spanish docs now available';
-      //     const message = this.lang === 'zh-CN'
-      //       ? '点击这里进行切换'
-      //       : 'Click here to switch';
-      //     const self = this;
-      //     this.$notify({
-      //       title,
-      //       duration: 0,
-      //       message,
-      //       onClick() {
-      //         self.$router.push('/es');
-      //         localStorage.setItem('ES_NOTIFIED_2', 1);
-      //       },
-      //       onClose() {
-      //         localStorage.setItem('ES_NOTIFIED_2', 1);
-      //       }
-      //     });
-      //   }
-      // }, 3500);
     }
   };
 </script>
